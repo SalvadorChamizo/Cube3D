0ed2f85745ff7e8a81ec9eb9b115657abdae5712
Salvador Chamizo <salvadorchamizo@gmail.com>
2024-08-19 18:34:32 +0200

    Setting up the project

---
+++ include/cub3d.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: schamizo <schamizo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/19 18:26:26 by schamizo          #+#    #+#             */
+/*   Updated: 2024/08/19 18:30:59 by schamizo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <string.h>
+# include <math.h>
+
+#endif
