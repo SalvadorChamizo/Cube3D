9aa90eaffe40817a97f0a35b7a5a118f7f98c213
Salvador Chamizo <salvadorchamizo@gmail.com>
2024-08-20 13:08:56 +0200

    Functional MLX42

--- MLX42/MLX42/CMakeFiles/Makefile.cmake
+++ MLX42/MLX42/CMakeFiles/Makefile.cmake
@@ -7,11 +7,11 @@ set(CMAKE_DEPENDS_GENERATOR "Unix Makefiles")
 # The top level Makefile was generated from the following files:
 set(CMAKE_MAKEFILE_DEPENDS
   "CMakeCache.txt"
+  "../CMakeLists.txt"
   "CMakeFiles/3.22.1/CMakeCCompiler.cmake"
   "CMakeFiles/3.22.1/CMakeCXXCompiler.cmake"
   "CMakeFiles/3.22.1/CMakeSystem.cmake"
-  "CMakeLists.txt"
-  "cmake/Findglfw3.cmake"
+  "../cmake/Findglfw3.cmake"
   "/usr/share/cmake-3.22/Modules/CMakeCCompiler.cmake.in"
   "/usr/share/cmake-3.22/Modules/CMakeCCompilerABI.c"
   "/usr/share/cmake-3.22/Modules/CMakeCInformation.cmake"
