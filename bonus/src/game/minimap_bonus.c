9917fb43b271bffb1aef343a59a5e9941fa42efb
Sergio Aroca <saroca1993@gmail.com>
2024-09-17 18:37:49 +0200

    minimap added

--- bonus/src/game/minimap_bonus.c
+++ bonus/src/game/minimap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: saroca-f <saroca-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 12:15:57 by saroca-f          #+#    #+#             */
-/*   Updated: 2024/09/17 11:22:12 by saroca-f         ###   ########.fr       */
+/*   Updated: 2024/09/17 18:28:43 by saroca-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,10 +39,27 @@ bool	check_point(int x, int y)
 	return (false);
 }
 
+void	cono_printer(t_data *data, t_ray *ray)
+{
+	double	x;
+	double	y;	
+
+	x = 160;
+	y = 160;
+	while(check_point((int)(y), (int)(x)))
+	{
+		//if (check_point((int)(data->player.pos_y + y), (int)(data->player.pos_x + x)))
+		mlx_put_pixel(data->walls, (int)x, (int)y, 0xFF000088);
+		x += ray->ray_dir_x;
+		y += ray->ray_dir_y;
+	}
+}
+
 void    make_minimap(t_data *data)
 {
 	int x = 0;
 	int y = 0;
+	int i = 0;
 
 	while (y < 320)
 	{
@@ -50,13 +67,16 @@ void    make_minimap(t_data *data)
 		while(x < 320)
 		{
 			if (check_wall(data, x, y))
-				mlx_put_pixel(data->walls, x, y, 0xFF000088);
+				mlx_put_pixel(data->walls, x, y, 0xFF00FF88);
 			else if (check_point(x, y))
 				mlx_put_pixel(data->walls, x, y, 0x00FF0088);
-			//else if (cone_check(data, x, y))
-			//	mlx_put_pixel(data->walls, x, y, 0x0000FF88);
 			x++;
 		}
 		y++;
+	}
+	while (i < 1920)
+	{
+		cono_printer(data, &data->player.ray[i]);
+		i++;
 	}
 }
